540c2416f064dd49c5bd3de77738abd9853e993a
NoelK55 <99165712+NoelK55@users.noreply.github.com>
2022-03-10 12:38:55 +0200

    H1.0

--- h1.0/pizzasave.js
+++ h1.0/pizzasave.js
@@ -1,3 +1,49 @@
+document.addEventListener("DOMContentLoaded", alku);
+
+function alku()
+{
+    if(localStorage.getItem("pepperonipizza") === "kylla")
+    {
+        document.getElementById("pepperonimenu_lista")
+    }
+    else {
+        document.getElementById("pepperonimenu_lista").style.display = "none";
+    }
+
+    if(localStorage.getItem("mexicanapizza") === "kylla")
+    {
+        document.getElementById("mexicanamenu_lista")
+    }
+    else {
+        document.getElementById("mexicanamenu_lista").style.display = "none";
+    }
+
+    if(localStorage.getItem("americanapizza") === "kylla")
+    {
+        document.getElementById("americanamenu_lista")
+    }
+    else {
+        document.getElementById("americanamenu_lista").style.display = "none";
+    }
+
+    if(localStorage.getItem("kokinpizza") === "kylla")
+    {
+        document.getElementById("kokinmenu_lista")
+    }
+    else {
+        document.getElementById("kokinmenu_lista").style.display = "none";
+    }
+
+    if(localStorage.getItem("talonpizza") === "kylla")
+    {
+        document.getElementById("talonmenu_lista")
+    }
+    else {
+        document.getElementById("talonmenu_lista").style.display = "none";
+    }
+
+    
+}
 
 
 function pepperoni()
@@ -8,9 +54,9 @@ function mexicana()
 {
     localStorage.setItem("mexicanapizza", "kylla");
 }
-function karon()
+function americana()
 {   
-    localStorage.setItem("karonpizza", "kylla");
+    localStorage.setItem("americanapizza", "kylla");
 }
 function kokin()
 {
@@ -20,12 +66,11 @@ function talontapaan()
 {
     localStorage.setItem("talonpizza", "kylla");
 }
-
 function valmistilaus()
 {
     localStorage.setItem("pepperoniluku", document.getElementById("nppizza").value);
     localStorage.setItem("mexicanaluku", document.getElementById("nmexipizza").value);
-    localStorage.setItem("karoluku", document.getElementById("nkaropizza").value);
+    localStorage.setItem("americanaluku", document.getElementById("namericanapizza").value);
     localStorage.setItem("kokinluku", document.getElementById("nkokkipizza").value);
     localStorage.setItem("talontapaanluku", document.getElementById("ntalopizza").value);
 }
